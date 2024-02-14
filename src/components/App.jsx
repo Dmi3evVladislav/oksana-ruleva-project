6c7fba1921bc6091ee5ef297a9d025e7fe181f37
Vladislav Dmitriev <72341764+Dmi3evVladislav@users.noreply.github.com>
2024-02-15 00:12:42 +0300

    Creating family tree
    -Creating layout and base for project
    -- Used SVGR to convert Figma exported SVG to react

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+//import Card from './Card/Card'
+import Table from './Table/Table'
+
+const App = () => {
+  return (
+    <main className='app' id='app'>
+      <Table className={`table`}/>
+      
+        {/* <div className="app-first_row">
+            <Card>Мама</Card>
+            <Card>Мама</Card>
+        </div> */}
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div className='card-block'>{props.children}</div>
+  )
+}
+
+export default Card
